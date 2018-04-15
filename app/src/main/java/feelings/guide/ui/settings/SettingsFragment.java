464c03337bcfbcd3c558071c345c91dd0e4e0beb
dorzhinov <dorzhinov@gmail.com>
2018-04-15 18:12:25 -0500

    Added toasts on date and time format change. Changed labels in preferences.

--- app/src/main/java/feelings/guide/ui/settings/SettingsFragment.java
+++ app/src/main/java/feelings/guide/ui/settings/SettingsFragment.java
@@ -18,9 +18,11 @@ import static android.app.Activity.RESULT_OK;
 
 public class SettingsFragment extends PreferenceFragment {
 
+    public static final String DATE_FORMAT_KEY = "Feelings.Guide.Date.Format";
+    public static final String TIME_FORMAT_KEY = "Feelings.Guide.Time.Format";
+    private static final String RESTORE_BUILT_IN_QUESTIONS_KEY = "Feelings.Guide.Restore.Built.In.Questions";
     private static final String CLEAR_LOG_FULL_KEY = "Feelings.Guide.Clear.Log.Full";
     private static final String CLEAR_LOG_DELETED_KEY = "Feelings.Guide.Clear.Log.Deleted";
-    private static final String RESTORE_BUILT_IN_QUESTIONS_KEY = "Feelings.Guide.Restore.Built.In.Questions";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,9 +30,66 @@ public class SettingsFragment extends PreferenceFragment {
 
         addPreferencesFromResource(R.xml.preferences);
 
+        setUpDateFormatPreference();
+        setUpTimeFormatPreference();
+        setUpRestoreBuiltInQuestionsPreference();
         setUpClearLogFullPreference();
         setUpClearLogDeletedPreference();
-        setUpRestoreBuiltInQuestionsPreference();
+    }
+
+    private void setUpDateFormatPreference() {
+        Preference dateFormatPreference = getPreferenceScreen().findPreference(DATE_FORMAT_KEY);
+        dateFormatPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                Activity activity = getActivity();
+                if (activity != null) {
+                    ToastUtil.showShort(activity, getString(R.string.msg_date_format_success));
+                }
+                return true;
+            }
+        });
+    }
+
+    private void setUpTimeFormatPreference() {
+        Preference dateFormatPreference = getPreferenceScreen().findPreference(TIME_FORMAT_KEY);
+        dateFormatPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                Activity activity = getActivity();
+                if (activity != null) {
+                    ToastUtil.showShort(activity, getString(R.string.msg_time_format_success));
+                }
+                return true;
+            }
+        });
+    }
+
+    private void setUpRestoreBuiltInQuestionsPreference() {
+        Preference clearLogPreference = getPreferenceScreen().findPreference(RESTORE_BUILT_IN_QUESTIONS_KEY);
+        clearLogPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                final Activity activity = getActivity();
+                new AlertDialog.Builder(activity)
+                        .setMessage(R.string.title_confirm_restore_built_in_questions_dialog)
+                        .setPositiveButton(R.string.btn_restore, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                QuestionService.restoreHidden(activity);
+                                ToastUtil.showShort(activity, getString(R.string.msg_restore_built_in_questions_success));
+                                Intent data = new Intent();
+                                data.putExtra(QuestionsActivity.REFRESH_QUESTIONS_KEY, true);
+                                activity.setResult(RESULT_OK, data);
+                            }
+                        })
+                        .setNegativeButton(R.string.btn_cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        }).create()
+                        .show();
+                return true;
+            }
+        });
     }
 
     private void setUpClearLogFullPreference() {
@@ -68,33 +127,6 @@ public class SettingsFragment extends PreferenceFragment {
                             public void onClick(DialogInterface dialog, int id) {
                                 AnswerStore.deleteForDeletedQuestions(activity);
                                 ToastUtil.showShort(activity, getString(R.string.msg_clear_log_full_success));
-                            }
-                        })
-                        .setNegativeButton(R.string.btn_cancel, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.dismiss();
-                            }
-                        }).create()
-                        .show();
-                return true;
-            }
-        });
-    }
-
-    private void setUpRestoreBuiltInQuestionsPreference() {
-        Preference clearLogPreference = getPreferenceScreen().findPreference(RESTORE_BUILT_IN_QUESTIONS_KEY);
-        clearLogPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            public boolean onPreferenceClick(Preference preference) {
-                final Activity activity = getActivity();
-                new AlertDialog.Builder(activity)
-                        .setMessage(R.string.title_confirm_restore_built_in_questions_dialog)
-                        .setPositiveButton(R.string.btn_restore, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                QuestionService.restoreHidden(activity);
-                                ToastUtil.showShort(activity, getString(R.string.msg_restore_built_in_questions_success));
-                                Intent data = new Intent();
-                                data.putExtra(QuestionsActivity.REFRESH_QUESTIONS_KEY, true);
-                                activity.setResult(RESULT_OK, data);
                             }
                         })
                         .setNegativeButton(R.string.btn_cancel, new DialogInterface.OnClickListener() {
--- app/src/main/java/feelings/guide/util/DateTimeUtil.java
+++ app/src/main/java/feelings/guide/util/DateTimeUtil.java
@@ -6,20 +6,20 @@ import android.preference.PreferenceManager;
 
 import org.threeten.bp.format.DateTimeFormatter;
 
+import static feelings.guide.ui.settings.SettingsFragment.DATE_FORMAT_KEY;
+import static feelings.guide.ui.settings.SettingsFragment.TIME_FORMAT_KEY;
 import static org.threeten.bp.format.DateTimeFormatter.ofPattern;
 
 public class DateTimeUtil {
 
     public static final DateTimeFormatter DB_FORMATTER = ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
-    public static final String ANSWER_LOG_DATE_DEFAULT_FORMAT = "d MMM yyyy";
-    public static final String ANSWER_LOG_TIME_DEFAULT_FORMAT = "HH:mm";
-    public static final String DATE_FORMAT_PREFERENCE = "Feelings.Guide.Date.Format";
-    public static final String TIME_FORMAT_PREFERENCE = "Feelings.Guide.Time.Format";
+    private static final String ANSWER_LOG_DATE_DEFAULT_FORMAT = "d MMM yyyy";
+    private static final String ANSWER_LOG_TIME_DEFAULT_FORMAT = "HH:mm";
 
     public static String getDateTimeFormat(Context context) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        String dateFormat = preferences.getString(DATE_FORMAT_PREFERENCE, ANSWER_LOG_DATE_DEFAULT_FORMAT);
-        String timeFormat = preferences.getString(TIME_FORMAT_PREFERENCE, ANSWER_LOG_TIME_DEFAULT_FORMAT);
+        String dateFormat = preferences.getString(DATE_FORMAT_KEY, ANSWER_LOG_DATE_DEFAULT_FORMAT);
+        String timeFormat = preferences.getString(TIME_FORMAT_KEY, ANSWER_LOG_TIME_DEFAULT_FORMAT);
         return dateFormat + ", " + timeFormat;
     }
 }
