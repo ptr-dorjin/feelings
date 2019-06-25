bcbb8ebc5c2148f49b1f46e7b79a1e4b0a6f7df4
Peter <dorzhinov@gmail.com>
2019-06-25 01:24:11 -0500

    Added UI tests Feelings selection //flaky

--- app/src/androidTest/kotlin/feelings/guide/ui/ClearLogUITest.kt
+++ app/src/androidTest/kotlin/feelings/guide/ui/ClearLogUITest.kt
@@ -4,12 +4,8 @@ import android.content.Context
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
---
+++ app/src/androidTest/kotlin/feelings/guide/ui/FeelingsListUITest.kt
@@ -0,0 +1,50 @@
+package feelings.guide.ui
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.FlakyTest
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import feelings.guide.R
+import feelings.guide.ui.question.QuestionsActivity
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@FlakyTest(detail = "Fails on Android 5, 7, 8. Passes on Android 4, 6, 9")
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class FeelingsListUITest {
+    private lateinit var context: Context
+
+    @get:Rule
+    var activityRule = ActivityTestRule(QuestionsActivity::class.java)
+
+    @Before
+    fun before() {
+        context = getApplicationContext()
+    }
+
+    @Test
+    fun selectFeelingWithScroll_closesAnswerActivity() {
+        // given
+        val feeling = context.resources.getStringArray(R.array.joy_array)[3]
+
+        // when
+        addFeelingsAnswer(R.string.joy, feeling)
+
+        // then
+        onView(withId(R.id.questionRV)).check(matches(isDisplayed()))
+    }
+}
