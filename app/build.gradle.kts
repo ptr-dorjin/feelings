97167eb2ddb692e06216ade9e5d70e87486efb57
Peter <dorzhinov@gmail.com>
2019-08-09 21:52:58 -0500

    bumped up app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "feelings.guide"
         minSdkVersion(15)
         targetSdkVersion(28)
-        versionCode = 3
-        versionName = "2.0.0"
+        versionCode = 4
+        versionName = "2.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
