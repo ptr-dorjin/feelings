a648a40af308fc5430da1b6a86f565afaac14b58
Petr Dorzhinov <dorzhinov@gmail.com>
2020-12-26 17:59:30 -0600

    Prepare for the next release - 2.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "feelings.guide"
         minSdkVersion(19)
         targetSdkVersion(30)
-        versionCode = 7
-        versionName = "2.2.1"
+        versionCode = 8
+        versionName = "2.2.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         multiDexEnabled = true
